03de2752ad6139fab5e7916a65167cf3aeb8b475
echo <hujw77@gmail.com>
2022-06-10 12:25:55 +0800

    debug

--- packages/beacon-state-transition/src/util/signingRoot.ts
+++ packages/beacon-state-transition/src/util/signingRoot.ts
@@ -1,15 +1,15 @@
 import {Domain, phase0, ssz} from "@chainsafe/lodestar-types";
-import {Type} from "@chainsafe/ssz";
+import {Type, toHexString} from "@chainsafe/ssz";
 
 /**
  * Return the signing root of an object by calculating the root of the object-domain tree.
  */
 export function computeSigningRoot<T>(type: Type<T>, sszObject: T, domain: Domain): Uint8Array {
-  console.log(3, type.hashTreeRoot(sszObject))
+  console.log(3, toHexString(type.hashTreeRoot(sszObject)))
   const domainWrappedObject: phase0.SigningData = {
     objectRoot: type.hashTreeRoot(sszObject),
     domain,
   };
-  console.log(4, ssz.phase0.SigningData.hashTreeRoot(domainWrappedObject))
+  console.log(4, toHexString(ssz.phase0.SigningData.hashTreeRoot(domainWrappedObject)))
   return ssz.phase0.SigningData.hashTreeRoot(domainWrappedObject);
 }
